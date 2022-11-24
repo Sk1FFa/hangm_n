51fcb3fb3734b49773c12cd3b4c027a4ab81db07
Sk1FFa <Sk1FFa@mail.ru>
2022-11-24 09:57:54 +0700

    first commit

---
+++ Lab2 RPM/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Lab2_RPM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string? NickName;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NicknameTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox NicknameTextBox = (TextBox)sender;
+            NicknameTextBox.Text = string.Empty;
+            NicknameTextBox.GotFocus -= NicknameTextBox_GotFocus;
+            NicknameTextBox.Foreground = Brushes.Black;
+        }
+
+        private void StartGame_Click(object sender, RoutedEventArgs e)
+        {
+            NickName = NicknameTextBox.Text;
+            GameWindow GW = new GameWindow(NickName);
+            GW.Show();
+            this.Hide();
+        }
+    }
+}
